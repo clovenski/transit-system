7c2c6b0796fbf315783e149688dac2ab37d82af0
Joel Tengco <joeltengco@yahoo.com>
2019-04-23 22:21:54 -0700

    edit run script to support mac os

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-java -cp '.;bin;res/*' Lab4
+if [[ "$OSTYPE" == "msys" ]]; then
+  java -cp '.;bin;res/*' Lab4
+elif [[ "$OSTYPE" == "darwin" ]]; then
+  java -cp '.:bin:res/*' Lab4
+else
+  echo "script does not support your os: $OSTYPE"
+  echo "edit the script to adjust"
+fi
